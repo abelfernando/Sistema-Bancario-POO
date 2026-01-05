859337b96a513c40e6137ff8332c35a9307ad44f
Abel Souza <abel.fernando@yahoo.com.br>
2026-01-05 19:41:59 -0300

    Implementado classe conta corrente

--- sistema_bancario_POO.py
+++ sistema_bancario_POO.py
@@ -86,6 +86,32 @@ class ContaCorrente(Conta):
         self.limite = limite
         self.limite_saques = limite_saques
 
+    def sacar(self, valor):
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__]
+        )
+
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saques >= self.limite_saques
+
+        if excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        else:
+            return super().sacar(valor)
+
+        return False
+    
+    def __str__(self):
+        return f"""\
+            Agência:\t{self.agencia}
+            C/C:\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
+
 def deposito(saldo, valor, extrato, /):
     """Realiza um depósito na conta bancária.
     Parâmetros apenas por posição: (saldo, valor, extrato)"""
